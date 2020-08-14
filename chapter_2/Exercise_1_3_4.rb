05d9fb3e19eeea2b119651f4851c6c4fac885d9d
Rodshell Fleurinord <rodshellfleurinord@gmail.com>
2020-08-14 21:43:54 +0000

    These are the exercises for chapter_2

---
+++ chapter_2/Exercise_1_3_4.rb
@@ -0,0 +1,6 @@
+#name.rb
+puts "Please input your first name below."
+first_name = gets.chomp
+puts "Please input your last name below."
+last_name = gets.chomp
+10.times { puts "Hello #{first_name} #{last_name}!" }
